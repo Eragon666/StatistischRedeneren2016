3b7015169ca361998239e37efaee2fdf2d7ceca7
Kwan Win Chung <superkwan-win@hotmail.com>
2016-04-15 14:11:12 +0200

    IK HAAT COMMIT MESSAGES

---
+++ Week3/Inverse_Transform_Sampling.py
@@ -0,0 +1,22 @@
+import numpy as np
+import math
+from matplotlib import pyplot as plt
+
+lamda = 1
+random_list = []
+
+# The main loop that generates random numbers
+for i in range(1000000):
+    random_number = np.random.uniform()
+    result = math.fabs((1/lamda) * math.log(random_number))
+    random_list.append(result)
+
+# estimate lambda
+mean_x = np.mean(random_list)
+lambda_schatting = 1/mean_x
+print 'Estimated lambda', lambda_schatting
+
+# Plot all the random numbers in a histogram.
+bins = 15
+plt.hist(random_list, bins, range=[0, bins], normed=True)
+plt.show()
