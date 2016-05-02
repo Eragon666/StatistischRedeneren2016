7fa344cff673b990670969a9b077c3106e5cabb3
Matthijs <matthijsthoolen@hotmail.com>
2016-05-02 17:41:52 +0200

    Opgave 22 af

--- Week4/Opdracht_22.py
+++ Week4/Opdracht_22.py
@@ -1,51 +1,58 @@
+#######
+# Opdracht 22
+# Door: Kwan Win Chung, Younes en Matthijs Thoolen
+# Datum: 2-5-2016
+#######
+
 from pylab import *
 
-def meanAndCovariance(mu, Sigma):
+def meanAndCovariance(mu, Sigma, length):
 
-    # First generate data set with known mu and Sigma
+    # With the known mu and sigma we generate the data set
     d, U = eig(Sigma)
     L = diagflat(d)
     A = dot(U, sqrt(L))
-    X = randn(4, 1000)
-    # Our data matrix is Y
-    Y = dot(A,X) + tile(mu, 1000)
-
-    # Calculate mean
-    Ybar = mean(Y, 1)
-
-    # subtract mean from each column
-    Yzm = Y - tile(Ybar[:,newaxis], 1000)
-    # Estimate covariance matrix
-    S = dot(Yzm, transpose(Yzm)) / 999
-    print("Estimated mean:", Ybar)
-    print("Estimated Covariance matrix:", S)
+    X = randn(4, length)
     
-def calcMu(mu, Sigma):
-    # An array that will hold the estimations of the mean
-    collect = np.zeros((1000, 4))
+    # The data matrix
+    Y = dot(A,X) + tile(mu, length)
 
-    # First generate data set with known mu and Sigma
+    Mean = mean(Y, 1)
+
+    # We need to subtract the mean from each column
+    Yzm = Y - tile(Mean[:,newaxis], length)
+    
+    # Estimate covariance matrix
+    CoMa = dot(Yzm, transpose(Yzm)) / (length-1)
+    
+    return (Mean, CoMa)
+    
+def calcMu(mu, Sigma, length):    
+    # With the known mu and sigma we generate the data set
     d, U = eig(Sigma)
     L = diagflat(d)
     A = dot(U, sqrt(L))
     X = randn(4, 1000)
     Y = dot(A,X) + tile(mu, 1000)
+    
+    # An array that will hold the estimations of the mean
+    collection = np.zeros((length, 4))
 
     # Calculate the mean values and put it in a data matrix
-    for x in range (0, 1000):
+    for x in range (0, length):
 	    Mean = mean(Y, 1)
-	    collect[x] = Ybar
-
-    collect = collect.reshape(4, 1000)
-    Y = collect
-    Mean = mean(Y, 1)
-    # subtract mean from each column
-    Yzm = Y - tile(Ybar[:, newaxis], 1000)
+	    collection[x] = Mean   
+    
+    reshape = collection.reshape(4, 1000)
+    Mean = mean(reshape, 1)
+    
+    # We need to subtract the mean from each column
+    Yzm = reshape - tile(Mean[:, newaxis], 1000)
+    
     # Estimate covariance matrix
     CoMa = dot(Yzm, transpose(Yzm)) / 999
     
-    print("Mean (Estimated):\n", Mean)
-    print("Covariance matrix (Estimated):\n", CoMa)
+    return (Mean, CoMa);
     
 def main():
 
@@ -55,9 +62,16 @@ def main():
 	    [ -2.08792829,   0.48723704,  -1.59255406,   8.28742469]] )
 	    
     mu = array([[1],[2],[3],[4]])
+    
+    length = 10**3
 
-    meanAndCovariance(mu, Sigma)
-    calcMu(mu, Sigma)
+    #result1 = meanAndCovariance(mu, Sigma, length)
+    #print("Mean (Estimated):", result1[0])
+    #print("Covariance matrix (Estimated):", result1[1])
+    
+    result2 = calcMu(mu, Sigma, length)
+    print("Mean (Estimated):", result2[0])
+    print("Covariance matrix (Estimated):", result2[1])
 
 if __name__ == "__main__":
     main()
